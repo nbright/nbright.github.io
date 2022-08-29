71dce1afe3450e2803a33dc8c526824d7db272d6
Minsung <kms3700@gmail.com>
2022-08-29 10:46:00 +0900

    배경중앙, 레이아웃 정리

--- js/todo.js
+++ js/todo.js
@@ -18,7 +18,7 @@ function saveToDos(){
 }
 
 function deleteToDo(event){
-    const li = event.target.parentElement;
+    const li = event.target.parentElement.parentElement.parentElement.parentElement;
     console.log(li.id);
     toDos=toDos.filter(toDo => toDo.id !== parseInt(li.id));
     li.remove();
@@ -31,11 +31,22 @@ function paintToDo(newTodoObj){
     const span = document.createElement("span");
     span.innerText=newTodoObj.text;
 
-    const button = document.createElement("button");
-    button.innerText="⨉";
-    button.addEventListener("click", deleteToDo);
+     //const button = document.createElement("button");
+    const div = document.createElement("div");
+    div.innerHTML="<div class='main-button-out'><div class='main-button-in'><button>⨉</button></div></div>"
+    div.addEventListener("click", deleteToDo);
+
+    /*    button.innerText="⨉"
+        button.wrap( '<div class="main-button-out"><div class="main-button-in"></div></div>' );
+        button.innerHTML="<div class='test'>⨉</div>"*/
+
+        // button.innerText="X";
+        // button.addEventListener("click", deleteToDo);
+
+         // button.innerText="X";
+         // button.addEventListener("click", deleteToDo);
     li.appendChild(span);
-    li.appendChild(button);
+    li.appendChild(div);
     toDoList.appendChild(li);
     
 }
